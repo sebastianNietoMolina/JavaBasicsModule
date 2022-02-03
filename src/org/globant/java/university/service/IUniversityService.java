def2a6e65772836d05bdb4067b5e60f542f9c717
sebastianNietoMolina <juan.nieto-mo@mail.escuelaing.edu.co>
2022-02-03 18:38:16 -0500

    Adding service layer

---
+++ src/org/globant/java/university/service/IUniversityService.java
@@ -0,0 +1,31 @@
+package org.globant.java.university.service;
+
+import org.globant.java.university.model.Student;
+import org.globant.java.university.model.Subject;
+import org.globant.java.university.model.fathers.Teacher;
+
+import java.util.List;
+
+public interface IUniversityService {
+
+    void addTeacherToUniversity(Teacher teacher);
+
+    void addStudentToUnivesity(Student student);
+
+    void addSubjectToUniveresity(Subject subject);
+
+    void addStudentToSubject(Student student, int subjectId);
+
+    List<Teacher> getTeachers();
+
+    List<Subject> getSubjects();
+
+    List<Student> getStudents();
+
+    Subject getSubjectById(int id);
+
+    Teacher getTeacherById(int id);
+
+    List<Subject> getSubjectsByGivenStudent(Student student);
+
+}
